ffb7c3bd9d2d8766244c073ef8fb0e87f404072f
kavyashreeranganathan <106268447+kavyashreeranganathan@users.noreply.github.com>
2024-05-09 09:27:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>ETMS</title>
+    <!-- jQuery file -->
+    <script src="includes/juqery_latest.js"></script>
+    <!-- Bootstrap files -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- External CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="row">
+        <div class="col-md-4 m-auto" id="home_page">
+            <center><h3>Choose login role ?</h3><br>
+            <a href="user_login.php" class="btn btn-success" style="margin-right: 20px;">User Login</a>
+            <a href="register.php" class="btn btn-warning" style="margin-right: 20px;">User Registration</a>
+            <a href="admin/admin_login.php" class="btn btn-info">Admin Login</a></center>
+        </div>
+    </div>
+</body>
+</html>
